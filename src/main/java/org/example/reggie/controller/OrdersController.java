2e2a051fea41c8f2e3d0746462d070eefaf75019
刘成龙 <2774623562>
2022-11-07 09:20:01 +0800

    添加了用户订单分页查询

--- src/main/java/org/example/reggie/controller/OrdersController.java
+++ src/main/java/org/example/reggie/controller/OrdersController.java
@@ -3,13 +3,12 @@ package org.example.reggie.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.example.reggie.common.BaseContext;
 import org.example.reggie.common.R;
 import org.example.reggie.entity.Orders;
 import org.example.reggie.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 /**
  * @description:
@@ -62,14 +61,25 @@ public class OrdersController {
     }
 
     /**
-     *设置快捷
+     *用户订单分页查询
      * @return
      */
-    @GetMapping
-    public R<String> upload(){
-
-
-        return null;
+    @GetMapping("userPage")
+    public R<Page> userPage(int page,int pageSize){
+        log.info("page={},pageSize={}",page,pageSize);
+        //获取当前用户id
+        Long userId = BaseContext.getCurrentId();
+        //创建分页对象
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+        //创建条件查询对象
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //添加用户id条件
+        queryWrapper.eq(Orders::getUserId,userId);
+        //添加排序条件
+        queryWrapper.orderByAsc(Orders::getOrderTime);
+        //分页查询
+        ordersService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 
 
